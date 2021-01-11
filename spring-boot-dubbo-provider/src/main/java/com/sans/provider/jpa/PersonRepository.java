4b0606c307c83f3af3aaf9db72492add5994f304
张东彬(DJ009828) <dj009828zhangdongbin@djbx.com>
2021-01-11 16:06:42 +0800

    @Query

--- spring-boot-dubbo-provider/src/main/java/com/sans/provider/jpa/PersonRepository.java
+++ spring-boot-dubbo-provider/src/main/java/com/sans/provider/jpa/PersonRepository.java
@@ -2,6 +2,9 @@ package com.sans.provider.jpa;
 
  import com.sans.provider.entity.Person;
  import org.springframework.data.jpa.repository.JpaRepository;
+ import org.springframework.data.jpa.repository.Query;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
+ @Query(value = "SELECT * FROM PERSON WHERE ID = ?1", nativeQuery = true)
+ Person findByID(String ID);
 }
